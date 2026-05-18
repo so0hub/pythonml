5f6960e590d3e4a0af4c1c2da94f9191e3a8a5f4
박소영 <so0mile02@gmail.com>
2026-05-18 15:48:14 +0900

    250518 3:48

--- day02/T2-01.py
+++ day02/T2-01.py
@@ -15,4 +15,84 @@ import matplotlib.pyplot as plt
 plt.scatter( perch_length , perch_weight )
 plt.show()
 
-# [3] 학습 모델 만들기 , 준비
+# [3] 학습 모델 만들기 
+# (1) 준비 : 학습용과 테스트용 분리한다. 왜? 모델평가에 사용된다.
+from sklearn.model_selection import train_test_split
+# train_test_split( 학습자료 , 정답자료 , test_size=분리비율 , random_state=분리기준난수 )
+# random_state = 분리할 때 사용되는 난수값 , # 난수값에 따라 분리한다. 
+# 고정값 넣어주면 항상 동일한 분리값 넣을 수 있다. # 0~32억 사이 아무거나 , 관례적으로 42 많이 씀
+train_input , test_input , train_target , test_target = train_test_split( perch_length , perch_weight , test_size=0.3 , random_state=42 )
+
+# (2) 자료형식(모양) 구성 , 대부분 2차원 사용한다.
+import numpy as np
+array = np.array( [ 1 , 2 , 3 , 4 ] ) # 1차원
+print( array.shape ) # shape : 배열의 모양을 반환해줌 ( 행 , 열 ) , (4, ) : 4,0이라는뜻
+
+array2 = np.array( [ [1,2],[3,4],[5,6] ] ) # 2차원
+print( array2.shape ) # ( 행 , 열 ) , ( 3 , 2 )
+
+print( train_input.shape ) # (39,)      : 1차원 배열 --> 사이킷런 모델들은 1차원배열 학습이 불가능하다.
+print( train_input ) # 1차원으로 구성된 '농어' 길이
+# [17.4 36.  25.  40.  39.  43.  22.  20.  22.  24.  27.5 43.  40.  24. 21.  27.5 40.  32.8 26.5 36.5 13.7 22.7 15.  37.  35.  28.7 23.5 39. 21.  23.  22.  44.  22.5 19.  37.  22.  25.6 42.  34.5]
+
+# T1-01( zip활용 ), T1-02( column_stack 활용 ) , T2-01( reshape )   : 1차원 -> 2차원 만드는 것
+
+# [4]
+# .reshape( 행개수 , 열개수 ) :   행개수에는 -1 넣어서 자동으로 하겠다는 뜻 , 열개수는 1개
+train_input = train_input.reshape( -1 , 1 )
+print( train_input )
+print( train_input.shape ) # (39, 1)   : 2차원이 됐다.
+train_target = train_target.reshape( -1 , 1 )
+test_input = test_input.reshape( -1 , 1 )
+
+# [[17.4]
+#  [36. ]
+#  [25. ]
+#    ...
+#  [34.5]]
+
+
+# [5] 모델 학습
+from sklearn.neighbors import KNeighborsClassifier # K최근접이웃 모델 찾기
+from sklearn.neighbors import KNeighborsRegressor # K최근접이웃 회귀 모델
+knr = KNeighborsRegressor() # 모델 객체 생성
+knr.fit( train_input , train_target ) # 모델 학습 , # ( 길이 , 무게 ) # '길이'에 따른 '무게' 학습
+print( knr.score( test_input , test_target ) ) # 모델 평가 , 0.9929281790592219 # 회귀모델에서는 결정계수
+
+print( test_input )         # 모델 예측할 값 : [ 8.4  18  27.5  ] # 길이
+
+# [[  61.4]
+#  [  78. ]
+#  [ 248. ]
+#   . . .
+#   [ 126. ]
+#  [  92. ]]
+print( knr.predict( test_input ) ) # 모델( 무게 ) 예측 결과 
+# [[  61.4]
+#  [  78. ]
+#  [ 248. ]
+#  . . .
+#  [  92. ]]
+
+# [6] k최근접이웃 회귀는 이웃의 평균값으로 예측한다. 하이퍼라미터(k) 조절
+# k = 이웃 개수 정하기
+knr = KNeighborsRegressor() # 모델 객체 생성
+# 임의의 길이 생성 , 임의의 물고기 길이 5부터 45까지 생성
+x = np.arange( 5 , 45 ).reshape( -1 , 1 )
+print( x ) # 5 ~ 44 까지의 임의의값
+
+for k in [ 1 , 3 , 5 , 10 ]:                        
+    knr.n_neighbors = k                             
+    knr.fit( train_input , train_target )           
+    print( knr.score(test_input , test_target) )    
+    pred = knr.predict( x )                         
+    print( pred )                                   
+
+    # 시각화
+    plt.scatter( train_input , train_target )
+    plt.plot( x , pred )    # plot ( 선차트이면서  회귀(예측)선 )  # x = 길이  # pred = 몸무게(예측)
+    plt.title( f'k={k}')
+    plt.show()
+
+# k는 이웃개수 뜻한다. k최근접 회귀는 이웃의 평균으로 예측한다.
+# 
---
+++ day02/practice3.py
@@ -0,0 +1,65 @@
+# PythonML Practice3: K-최근접 이웃 회귀와 과소적합 해결 
+# https://www.kaggle.com/code/anshigupta01/iris-flower-classification
+
+# [단계 1] 데이터 로드 및 확인
+# 파일명: ./Iris.csv
+import pandas as pd
+df = pd.read_csv( './day02/Iris.csv' )
+df.info() # 잘 불러졌는지 확인
+
+# [단계 2] 특정 품종 추출 (데이터 필터링)
+# 전체 데이터 중 Species가 'Iris-setosa'인 데이터만 추출하세요.
+target_iris = df[ df['Species'].isin( ['Iris-setosa'] ) ]
+print( target_iris )
+
+# [단계 3] 특성 데이터 및 정답 데이터 추출
+# 추출한 붓꽃의 꽃받침 길이('SepalLengthCm')를 특성 데이터(iris_length)로, 
+# 꽃받침 너비('SepalWidthCm')를 정답 데이터(iris_width)로 추출하세요.
+iris_length = target_iris['SepalLengthCm'].values
+iris_width = target_iris[ 'SepalWidthCm'].values
+print( iris_length , iris_width ) # 길이 , 너비
+import matplotlib.pyplot as plt
+plt.scatter( iris_length , iris_width )
+plt.show()
+
+# [단계 4] 훈련용 / 테스트용 데이터 분리
+# train_test_split() 함수를 사용하여 학습용 데이터와 테스트용 데이터를 분리하세요.
+# test_size는 0.3, random_state는 42로 설정하세요.
+from sklearn.model_selection import train_test_split
+train_input , test_input , train_target , test_target = train_test_split( iris_length, iris_width, test_size=0.3 , random_state= 42 )
+
+import numpy as np
+
+# [단계 5] 데이터 차원 변환 (Reshape)
+# 사이킷런 모델 학습을 위해 1차원 배열인 train_input과 test_input을  [개수, 1] 형태의 2차원 배열로 변환하세요.
+train_input = np.array( train_input )
+print( train_input.shape ) # (35,) # 1차원
+
+train_input = train_input.reshape( -1 , 1 ) 
+print( train_input.shape ) # (35,1) # 2차원이 됐다
+# ===========================================
+test_input = np.array( test_input )
+print( test_input.shape ) # (15,) # 1차원
+
+test_input = test_input.reshape( -1 , 1 )
+print( test_input.shape ) # (15,1) # 2차원이 됐다
+
+
+# [단계 6] KNeighborsRegressor 모델 생성 및 학습
+# KNeighborsRegressor 객체를 생성하고 훈련용 데이터로 모델을 학습하세요.
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+knr = KNeighborsRegressor()
+knr.fit( train_input , train_target ) # 모델 학습
+p
+
+
+# [단계 7] 초기 모델 평가 및 결정계수(R^2) 확인
+# 훈련 세트와 테스트 세트의 정확도(score)를 각각 출력하기
+print( knr.score( test_input , test_target ) ) # 0.60355587808418
+print( knr.score( train_input , train_target ) ) # 0.575461861044996
+
+# [단계 8] 이웃 개수 변경에 따른 회귀선 시각화
+# 이웃의 개수(n_neighbors)가 1, 3, 5, 10으로 변화할 때, 꽃받침 길이 4.0부터 6.0까지의 구간에 대한 모델의 예측 회귀선을 각각 시각화하세요.
+
+# [단계 9] 단계8 에서 가장 적합한 이웃의 개수( 1, 3, 5, 10 중에)를 주석에 작성 하시오. 
